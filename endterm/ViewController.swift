f833f33556b856416fcbdbca0e1db58d34501ba5
Davran Arifzhanov <davron2001davron@gmail.com>
2020-12-08 09:16:54 +0600

    Initial Commit

---
+++ endterm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  endterm
+//
+//  Created by Davran Arifzhanov on 08.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
